59a6522bf80eb315b8d3b2639fde0c1f463373bf
Alberto <avillprojects@gmail>
2014-12-14 21:19:39 +0000

    updated nvm and commented out emacs

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.26
-nvm use v0.10.26
+nvm install v0.20.0
+nvm use v0.20.0
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -22,9 +22,9 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo apt-add-repository -y ppa:cassou/emacs
-sudo apt-get update
-sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+#sudo apt-add-repository -y ppa:cassou/emacs
+#sudo apt-get update
+#sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 # git pull and install dotfiles as well
 cd $HOME
@@ -39,7 +39,7 @@ ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+#ln -sf dotfiles/.emacs.d .
 
 ## Install Ruby RVM system
 \curl -L https://get.rvm.io | bash -s stable
